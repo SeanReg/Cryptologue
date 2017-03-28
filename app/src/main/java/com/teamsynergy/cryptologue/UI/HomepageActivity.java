b47cc07b9d41ee83c904d33c03fc34e7acefff4e
Matthew Redington <MatthewRedington@Matthews-MBP.attlocal.net>
2017-03-27 23:26:36 -0400

    My Message

--- app/src/main/java/com/teamsynergy/cryptologue/UI/HomepageActivity.java
+++ app/src/main/java/com/teamsynergy/cryptologue/UI/HomepageActivity.java
@@ -3,44 +3,42 @@ package com.teamsynergy.cryptologue.UI;
 /**
  * Created by MatthewRedington on 3/26/17.
  */
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.Toast;
-
+import android.widget.ArrayAdapter;
 import com.teamsynergy.cryptologue.R;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class HomepageActivity extends AppCompatActivity {
-    private List<String> mChatroomNameList = new ArrayList<>();
-    private RecyclerView mRecyclerView;
-    private RecyclerAdapter mAdapter;
+import static android.app.PendingIntent.getActivity;
 
-    @Override
+public class HomepageActivity extends AppCompatActivity {
+    private ListView mListView;
+    private ArrayList<String> mChatroomNameList = new ArrayList<>();
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
-        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
 
-        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-
-        mAdapter = new RecyclerAdapter(mChatroomNameList);
-
-        mRecyclerView.setHasFixedSize(true);
-        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
-        mRecyclerView.setLayoutManager(mLayoutManager);
-
-        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
-        mRecyclerView.setAdapter(mAdapter);
-
+        mListView = (ListView) findViewById(R.id.chatroom_list);
         prepChatroomData();
+
+
+        ChatroomListAdapter adapter = new ChatroomListAdapter(this, mChatroomNameList);
+        mListView.setAdapter(adapter);
+
     }
 
     private void prepChatroomData(){
@@ -56,11 +54,30 @@ public class HomepageActivity extends AppCompatActivity {
         mChatroomNameList.add(name);
         name= "test6";
         mChatroomNameList.add(name);
-
-
+        name= "test7";
+        mChatroomNameList.add(name);
+        name= "test8";
+        mChatroomNameList.add(name);
+        name= "test9";
+        mChatroomNameList.add(name);
+        name= "test10";
+        mChatroomNameList.add(name);
+        name= "test11";
+        mChatroomNameList.add(name);
+        name = "test12";
+        mChatroomNameList.add(name);
+        name= "test13";
+        mChatroomNameList.add(name);
+        name= "test14";
+        mChatroomNameList.add(name);
+        name= "test15";
+        mChatroomNameList.add(name);
+        name= "test16";
+        mChatroomNameList.add(name);
+        name= "test17";
+        mChatroomNameList.add(name);
+        name= "test18";
     }
-
-
 
 
 }
