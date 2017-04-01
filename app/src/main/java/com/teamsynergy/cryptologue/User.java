9c1d9da37c95397400256e805f46494840e60520
Sean Regan <regan.sean.p@gmail.com>
2017-04-01 01:41:36 -0400

    find users by phone number

--- app/src/main/java/com/teamsynergy/cryptologue/User.java
+++ app/src/main/java/com/teamsynergy/cryptologue/User.java
@@ -1,6 +1,12 @@
 package com.teamsynergy.cryptologue;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by jasonpinlac on 3/26/17.
@@ -44,4 +50,26 @@ public class User {
 
     public String getPhoneNumber() { return mPhoneNumber; }
 
+    public interface UsersFoundListener {
+        public void onUsersFound(List<User> users);
+    }
+
+    public static void findByPhoneNumber(List<String> numbers, final UsersFoundListener listener) {
+        ParseQuery<ParseUser> query = ParseUser.getQuery();
+        query.whereContainedIn(AccountManager.FIELD_PHONE_NUMBER, numbers);
+        query.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> objects, ParseException e) {
+                if (listener !=  null && e == null) {
+                    ArrayList<User> users = new ArrayList<User>();
+                    for (ParseUser usr : objects) {
+                        users.add(new User(usr.getUsername(), usr.getString(AccountManager.FIELD_DISPLAY_NAME),
+                                usr.getString(AccountManager.FIELD_PHONE_NUMBER), usr));
+                    }
+
+                    listener.onUsersFound(users);
+                }
+            }
+        });
+    }
 }
