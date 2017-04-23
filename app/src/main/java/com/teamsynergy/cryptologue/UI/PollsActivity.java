c8cf06a1c3a1a5359505db880da6860e5130386b
Matthew Redington <MatthewRedington@Matthews-MacBook-Pro.local>
2017-04-22 23:38:04 -0400

    Added the poll options to the poll event

--- app/src/main/java/com/teamsynergy/cryptologue/UI/PollsActivity.java
+++ app/src/main/java/com/teamsynergy/cryptologue/UI/PollsActivity.java
@@ -1,12 +1,18 @@
 package com.teamsynergy.cryptologue.UI;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.ValueDependentColor;
@@ -24,6 +30,10 @@ import java.util.Map;
  */
 
 public class PollsActivity extends AppCompatActivity{
+    private LayoutInflater mInflater;
+    private LinearLayout[] optionsArray= new LinearLayout[4];
+    private int mOptionSelected;
+    private Button buttonSubmit;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,5 +66,45 @@ public class PollsActivity extends AppCompatActivity{
 
         series.setDrawValuesOnTop(true);
         series.setValuesOnTopColor(Color.WHITE);
+
+
+        LinearLayout parent = (LinearLayout)findViewById(R.id.poll_result_layout);
+        for(int i = 0; i < 4; ++i) {
+            LinearLayout optionLayout = (LinearLayout) getLayoutInflater().inflate(R.layout.poll_option, null);
+            ((TextView)optionLayout.findViewById(R.id.option_texts)).setText("testing " + i);
+            parent.addView(optionLayout);
+            optionsArray[i]=optionLayout;
+        }
+
+        buttonSubmit = (Button) findViewById(R.id.submit_button);
+        buttonSubmit.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+               // mOptionSelected    //this is the selected option
+                finish();
+            }
+        });
+
+    }
+
+
+
+    public void onCheckboxClicked(View view) {
+        boolean checked = ((CheckBox) view).isChecked();
+        if(!checked)
+            return;
+        View parent= (View) view.getParent();
+
+        for(int i=0; i <optionsArray.length; ++i){
+            if(parent == optionsArray[i]){
+                mOptionSelected=i+1;
+            }
+            else{
+                CheckBox checkBox = (CheckBox)optionsArray[i].findViewById(R.id.option);
+                checkBox.setChecked(false);
+            }
+
+        }
+
     }
 }
